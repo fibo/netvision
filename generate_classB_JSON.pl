44caeafe22957132bfaf6345ccd6f0851ab217e7
fibo <casati_gianluca@yahoo.it>
2016-12-09 13:31:16 +0100

    fixed naming

--- generate_classB_JSON.pl
+++ generate_classB_JSON.pl
@@ -24,7 +24,7 @@ my $data_dir = &dataDir::forClassB($classB_subnet);
 my $aggregated_json_file = &jsonFile::forClassB($classB_subnet);
 
 # Exit if class B data file already exists on S3.
-my $aggregated_json_file_exists = `aws s3 ls s3://ip-v4.space/$aggregated_json_file_exists`;
+my $aggregated_json_file_exists = `aws s3 ls s3://ip-v4.space/$aggregated_json_file`;
 
 if ( $aggregated_json_file_exists ) {
     die "File s3://ip-v4.space/$aggregated_json_file already exists\n";
